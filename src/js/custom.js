6df1aeeda556d77bd5e102644e1e9df1d56d6fc3
Alka Suresh <alkasuresh@codeandhue.com>
2022-07-15 18:05:44 +0530

    first page is completed and pushing

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,1 +1,8 @@
-console.log("welcome Code and hue");
+// function initMap() {
+//     var location= {lat:10.015861,lng:76.341866};
+//      var map=new google.maps.Map(d.getElementById("map"),{
+//          zoom:4,
+//          center:location
+//      });
+// }
+    
